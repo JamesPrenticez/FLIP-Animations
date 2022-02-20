21f949d125fdd96bf87d3da27325fa4592351cd3
James Prentice <jamesprenticez@gmail.com>
2022-02-20 22:27:09 +1300

    Animate

--- components/FramerMotionGrid/FramerMotionGrid.jsx
+++ components/FramerMotionGrid/FramerMotionGrid.jsx
@@ -1,17 +1,18 @@
 import { useState, useEffect } from "react";
 import { data } from "../../data";
 import Link from "next/Link";
+import {motion, AnimatePresence} from 'framer-motion'
 //https://www.youtube.com/watch?v=nyg5Lpl6AiM
 
 import React from "react";
 
 function FramerMotionGrid() {
   const [items, setItems] = useState(data);
-  const [filtered, setFiltered] = useState(data)
+  const [filtered, setFiltered] = useState(items)
   const [active, setActive] = useState('all')
 
   useEffect(() => {
-    if(active === 0) setFiltered(items)
+    if(active === 'all') setFiltered(items)
     const filteredItems = items.filter((item) => item.tags.includes(active))
     setFiltered(filteredItems)
   }, [active])
@@ -21,7 +22,7 @@ function FramerMotionGrid() {
     <div className="w-full min-h-screen bg-black p-12">
       {/* Filter Buttons */}
       <div className="w-full h-full px-4 space-x-4 text-white p-6">
-        <button onClick={() => setFiltered(items)} className="w-32 h-16 border-2 border-white rounded-lg hover:text-black hover:bg-white">Show All</button>
+        <button onClick={() => setActive('all')} className="w-32 h-16 border-2 border-white rounded-lg hover:text-black hover:bg-white">Show All</button>
         <button onClick={() => setActive('hobby')} className="w-32 h-16 border-2 border-white rounded-lg hover:text-black hover:bg-white">Hobby</button>
         <button onClick={() => setActive('clone')} className="w-32 h-16 border-2 border-white rounded-lg hover:text-black hover:bg-white">Clone</button>
         <button onClick={() => setActive('database')} className="w-32 h-16 border-2 border-white rounded-lg hover:text-black hover:bg-white">Database</button>
@@ -30,55 +31,52 @@ function FramerMotionGrid() {
         <button onClick={() => setActive('ecommerce')} className="w-32 h-16 border-2 border-white rounded-lg hover:text-black hover:bg-white">Ecommerce</button>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 md:gap-4 p-2 md:pd-6">
+      <motion.div layout
+        className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 md:gap-4 p-2 md:pd-6"
+      >
         {/* Post Display Card */}
-        {filtered.map((item, index) => (
-          <Link key={index} href={`/`}>
-            <div
-              className="p-[2px] rounded-lg"
+        <AnimatePresence>
+        {filtered.map((item, index) => {
+          return (
+            <motion.div layout
+              key={index}
+              animate = {{ opacity: 1}}
+              initial = {{ opacity: 0}}
+              exit = {{ opacity: 0}}
+              className="p-[2px] rounded-lg h-full w-full"
               style={{backgroundImage: item.color}}
-            >
-              <div className="bg-white overflow-hidden rounded-lg">
-                <div className="group overflow-hidden">
-                  <img
-                    className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-300 ease-in-out cursor-pointer"
-                    src="/default-image.jpg"
+              >
+              <div className="overflow-hidden rounded-lg ">
+                <div 
+                  className="group overflow-hidden border-b-2"
+                  style={{borderColor: 'transparent'}}
+                >
+                  <Link href={`/`}>
+                    <img
+                      className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-300 ease-in-out cursor-pointer"
+                      src={item.image || "/default-image.jpg"}
                     />
+                  </Link>
                 </div>
 
-                <div className="p-2 h-[6rem]">
-                  <p className="text-[1.125rem] font-bold">{item.title}</p>
-                  <p className="text-[1rem]">{item.desc}</p>
+                <div className="p-2 h-[6rem] bg-black">
+                  <div 
+                    className="bg-clip-text text-transparent"
+                    style={{backgroundImage: item.color}}
+                  >
+                    <p className="text-[1.125rem] font-bold ">{item.title}</p>
+                    <p className="text-[1rem]">{item.desc}</p>
+                  </div>
                 </div>
               </div>
-            </div>
-          </Link>
-        ))}
-      </div>
+            </motion.div>
+            )
+          }
+        )}
+        </AnimatePresence>
+      </motion.div>
     </div>
   );
 }
 
 export default FramerMotionGrid;
-
-
-// <div
-//   key={index}
-//   className="p-4 m-4 rounded-md bg-blue-900 text-white h-4/6"
-// >
-//     <div className="flex w-full px-2 ">
-//       {/* Small Dot */}
-//       <div className="w-4 m-2 rounded-full" style={{background: item.color}}></div>
-//       <h3 className="text-2xl">{item.title}</h3>
-//     </div>
-
-//     {/* Image */}
-//     <div>
-//       <img className="w-full m-auto rounded-sm px-4 py-3" src='/default-image.jpg' alt="" />
-//     </div>
-
-//     {/* SubTitle */}
-//     <div className="px-4">
-//       <p className="italic text-gray-100 font-thin">{item.desc}</p>
-//     </div>
-// </div>
--- data.js
+++ data.js
@@ -3,31 +3,31 @@ export const data = [
     id: 1,
     title: "SelfRegulator",
     desc: "Track tasks every day of the week",
-    src: "/img/selfregulator.png",
+    image: "",
     tags: ['all', 'hobby', 'datebase', 'authentication'],
-    color: 'radial-gradient(circle, rgba(0,255,29,1) 0%, rgba(159,255,0,1) 100%)',
+    color: 'radial-gradient(circle, rgba(22,163,74,1) 0%, rgba(0,255,29,1) 100%)'
   },
   {
     id: 2,
     title: "Tea Time",
     desc: "Pick your favourite recipes for the week and generate a shopping list",
-    src: "/img/teatime.png",
+    image: "",
     tags: ['all', 'group', 'database'],
-    color: 'radial-gradient(circle, rgba(251,191,36,1) 0%, rgba(220,38,38,1) 100%)'
+    color: 'radial-gradient(circle, rgba(220,38,38,1) 0%, rgba(251,191,36,1) 100%)'
   },
   {
     id: 3,
     title: "Developer.Blog",
     desc: "Read, write and comment on blog posts",
-    src: "/img/blog.png",
+    image: "",
     tags: ['all', 'hobby', 'blog'],
-    color: 'radial-gradient(circle, rgba(250,204,21,1) 0%, rgba(22,163,74,1) 100%)'
+    color: 'radial-gradient(circle, rgba(22,163,74,1) 0%, rgba(0,255,29,1) 100%)'
   },
   {
     id: 4,
     title: "Instagram Clone",
     desc: "Post photos and have your friends like and comment", 
-    src: "/img/instagram.png",
+    image: "",
     tags: ['all', 'clone'],
     color: 'radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%,#d6249f 60%,#285AEB 90%)',
   },
@@ -35,15 +35,15 @@ export const data = [
     id: 5,
     title: "Google Doc's Clone",
     desc: "Rich text editor in the cloud",
-    src: "/img/googledocs.png",
+    image: "",
     tags: ['all', 'clone'],
-    color: 'radial-gradient(circle, rgba(56,189,248,1) 0%, rgba(37,99,235,1) 100%)'
+    color: 'radial-gradient(circle, rgba(37,99,235,1) 0%, rgba(56,189,248,1) 100%)'
   },
   {
     id: 6,
     title: "Poppins",
     desc: "Ecommerce Clothing Store",
-    src: "/img/poppins.png",
+    image: "",
     tags: ['all', 'hobby', 'ecommerce'],
     color: 'radial-gradient(circle, rgba(255,0,255,1) 0%, rgba(171,0,255,1) 83%)'
   },
@@ -51,17 +51,17 @@ export const data = [
     id: 7,
     title: "Egyptian NFT's",
     desc: "Mint a hero and fight the boss in a turn based card game",
-    src: "/img/egyptiannfts.png",
+    image: "/images/egyptian.png",
     tags: ['all', 'hobby', 'crypto'],
-    color: 'radial-gradient(circle, rgba(253,230,138,1) 0%, rgba(251,191,36,1) 100%)'
+    color: 'radial-gradient(circle, rgba(220,38,38,1) 0%, rgba(251,191,36,1) 100%)'
   },
   {
     id: 8,
     title: "ConJob",
     desc: "Upload & Download CSV to database",
-    src: "/img/conjob.png",
+    image: "",
     tags: ['all', 'hobby', 'database'],
-    color: 'radial-gradient(circle, rgba(37,99,235,1) 0%, rgba(30,64,175,1) 100%)'
+    color: 'radial-gradient(circle, rgba(37,99,235,1) 0%, rgba(56,189,248,1) 100%)'
   },
 
 ]
